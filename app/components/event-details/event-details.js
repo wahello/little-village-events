ceefc24d23837311b1c850ba5919e786276e622b
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-05-04 02:27:23 -0500

    Slightly less empty event details view

--- app/components/event-details/event-details.js
+++ app/components/event-details/event-details.js
@@ -1,13 +1,36 @@
+import { eventImage } from "../../utils/event";
 
 import { injectState } from "@textpress/freactal";
 
 import React from "react";
-import { View, Text } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
 import { compose } from "recompose";
 
-const EventDetails = ( { event } ) => (
+
+const Hero = ( { event, state } ) => {
+    const image = eventImage( event );
+    if ( !image )
+        return null;
+
+    const { windowDimensions: { width } } = state;
+    const height = Math.trunc( width / 1.85 );
+
+
+    const styles = StyleSheet.create( {
+        image: {
+            width,
+            height
+        }
+    } );
+
+    const uri = `${image}-/scale_crop/${width}x${height}/center/`;
+    return <Image style={ styles.image } source={ { uri } }/>;
+};
+
+const EventDetails = ( { event, state } ) => (
     <View>
-        <Text>Details for { event.name }</Text>
+        <Hero event={event} state={state}/>
+        <Text>{ event.name }</Text>
     </View>
 );
 
