5675c57ffe5c034b678275fba34a70055508ab3b
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-05-07 21:18:31 -0500

    WIP: factor out global state

--- app/screens.js
+++ app/screens.js
@@ -1,39 +1,16 @@
+import state from "./state";
+
 import EventDetails from "./components/event-details";
 import EventList from "./containers/event-list";
 
 import { provideState } from "@textpress/freactal";
 
-import { Dimensions } from "react-native";
 import { Navigation } from "react-native-navigation";
 import { compose } from "recompose";
 
-const state = {
-    windowDimensions: Dimensions.get( "window" ),
-    categories: {
-        "62": "Editors' Picks",
-        "63": "Music",
-        "64": "Art/Exhibition",
-        "65": "Theatre/Performance",
-        "81": "Literature",
-        "67": "Cinema",
-        "68": "Foodie",
-        "79": "Education",
-        "80": "Community",
-        "71": "Fashion",
-        "66": "Drink Specials",
-        "82": "Crafty",
-        "83": "Family",
-        "85": "Sports / Rec"
-    }
-};
-
-const stateProvider = {
-    initialState: () => state
-};
-
 export function registerScreens() {
-    Navigation.registerComponent( "events.list", () => compose( provideState( stateProvider ) )( EventList ) );
-    Navigation.registerComponent( "events.details", () => compose( provideState( stateProvider ) )( EventDetails ) );
+    Navigation.registerComponent( "events.list", () => compose( provideState( state ) )( EventList ) );
+    Navigation.registerComponent( "events.details", () => compose( provideState( state ) )( EventDetails ) );
 
     return {
         main: {
---
+++ app/state.js
@@ -0,0 +1,27 @@
+import { Dimensions } from "react-native";
+
+const initialState = {
+    windowDimensions: Dimensions.get( "window" ),
+    categories: {
+        "62": "Editors' Picks",
+        "63": "Music",
+        "64": "Art/Exhibition",
+        "65": "Theatre/Performance",
+        "81": "Literature",
+        "67": "Cinema",
+        "68": "Foodie",
+        "79": "Education",
+        "80": "Community",
+        "71": "Fashion",
+        "66": "Drink Specials",
+        "82": "Crafty",
+        "83": "Family",
+        "85": "Sports / Rec"
+    }
+};
+
+const state = {
+    initialState: () => initialState
+};
+
+export default state;
