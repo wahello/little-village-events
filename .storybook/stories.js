5089a5e299cc5bb43ea56bbb7e5126bc02daeac4
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-05-08 23:51:10 -0500

    Add event details storybook story

---
+++ .storybook/stories.js
@@ -0,0 +1,1 @@
+import "/app/containers/event-details/__stories__/event-details.story";
---
+++ app/containers/event-details/__stories__/event-details.story.js
@@ -0,0 +1,12 @@
+import EventDetails from "..";
+import event from "./event-1.json";
+
+import navigatorStyleDecorator from "/.storybook/decorators/navigator-style";
+
+import { storiesOf } from "@storybook/react-native";
+import React from "react";
+
+storiesOf( "EventDetailsScreen", module )
+    .addDecorator( navigatorStyleDecorator( "transparent" ) )
+    .add( "default", () => ( <EventDetails event={ event }/> ) )
+;
