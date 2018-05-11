8f4a084c456c671aa17f4ac861d992bf14019c26
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-05-11 18:57:18 -0500

    Pin icon

--- app/components/icons/index.js
+++ app/components/icons/index.js
@@ -2,3 +2,4 @@ import { Image } from "react-native";
 import React from "react";
 
 export const CalendarIcon = props => <Image source={require( "./calendar.png" )} {...props} />;
+export const PinIcon = props => <Image source={require( "./pin.png" )} {...props} />;
