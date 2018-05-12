475a11deabe3af82302e06688e6fbfe27f843d3c
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-05-11 19:06:31 -0500

    event-details-card

---
+++ app/components/event-details-card/event-details-card.js
@@ -0,0 +1,35 @@
+import { View, StyleSheet } from "react-native";
+import React from "react";
+
+const styles = StyleSheet.create( {
+    root: {
+        flexDirection: "row",
+        alignItems: "center",
+
+        alignSelf: "stretch",
+//        maxHeight: 64,
+
+        margin: 6,
+        padding: 10,
+
+        borderRadius: 10,
+        backgroundColor: "#ffffff",
+
+        shadowColor: "rgba(227, 227, 227, 0.38)",
+        shadowOffset: {
+            width: 0,
+            height: 8
+        },
+        shadowRadius: 30,
+        shadowOpacity: 1
+    }
+} );
+
+
+export default ( { style, children } ) => {
+    return (
+        <View style={ [ styles.root, style || {} ] }>
+            { children }
+        </View>
+    );
+}
--- app/components/event-details-date-card/event-details-date-card.js
+++ app/components/event-details-date-card/event-details-date-card.js
@@ -1,3 +1,5 @@
+import DetailsCard from "../event-details-card";
+
 import { CalendarIcon } from "../icons";
 
 import moment from "moment";
@@ -7,26 +9,7 @@ import React from "react";
 
 const styles = StyleSheet.create( {
     root: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "center",
-
-        alignSelf: "stretch",
-        maxHeight: 64,
-
-        margin: 6,
-        padding: 10,
-
-        borderRadius: 10,
-        backgroundColor: "#ffffff",
-
-        shadowColor: "rgba(227, 227, 227, 0.38)",
-        shadowOffset: {
-            width: 0,
-            height: 8
-        },
-        shadowRadius: 30,
-        shadowOpacity: 1
+        height: 64
     },
     icon: {
         paddingLeft: 6,
@@ -69,12 +52,12 @@ export default ( { event } ) => {
         return null;
 
     return (
-        <View style={ styles.root }>
+        <DetailsCard style={ styles.root } >
             <View style={ styles.icon }><CalendarIcon /></View>
             <View style={ styles.column }>
                 <Date date={starttime} />
                 <Time starttime={starttime} endtime={endtime} />
             </View>
-        </View>
+        </DetailsCard>
     );
 }
