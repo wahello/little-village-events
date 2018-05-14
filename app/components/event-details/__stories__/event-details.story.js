d0c4594413f0f2041457dd4e091c5719cce50f22
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-05-13 21:55:06 -0500

    Storybook: fix event details story

--- app/components/event-details/__stories__/event-details.story.js
+++ app/components/event-details/__stories__/event-details.story.js
@@ -4,12 +4,19 @@ import event from "./event-1.json";
 import navigatorStyleDecorator from "/.storybook/decorators/navigator-style";
 
 import { storiesOf } from "@storybook/react-native";
-import { injectState } from "@textpress/freactal";
+import { provideState, injectState } from "@textpress/freactal";
 
 import React from "react";
 import { compose } from "recompose";
 
-const Screen = compose( injectState )( EventDetails );
+
+const Screen = compose(
+    provideState( {
+        initialState: ( { event } ) => ( { event } )
+    } ),
+    injectState
+)( EventDetails );
+
 
 storiesOf( "EventDetailsScreen", module )
     .addDecorator( navigatorStyleDecorator( "transparent", { back: true } ) )
