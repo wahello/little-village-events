66838d6e4456de8074add63e8f7cdd1a5d3d0ebd
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-05-17 22:35:37 -0500

    WIP: web page toolbar icons

--- app/components/icons/index.js
+++ app/components/icons/index.js
@@ -5,4 +5,9 @@ export const CalendarIcon = props => <Image source={require( "./calendar.png" )}
 export const PinIcon = props => <Image source={require( "./pin.png" )} {...props} />;
 export const PhoneIcon = props => <Image source={require( "./phone.png" )} {...props} />;
 
+export const NextIcon = props => <Image source={require( "./next.png" )} {...props} />;
+export const PreviousIcon = props => <Image source={require( "./previous.png" )} {...props} />;
+export const ShareIcon = props => <Image source={require( "./share.png" )} {...props} />;
+export const SafariIcon = props => <Image source={require( "./safari.png" )} {...props} />;
+
 export const BackIcon = props => <Image source={require( "./back.png" )} {...props} />;
