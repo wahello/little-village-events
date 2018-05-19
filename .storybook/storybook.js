923f6225306ba00ebb29d7dd4c9cfa1cc62d1a66
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-05-18 21:01:28 -0500

    Storybook: add `withKnobs` decorator by default

--- .storybook/storybook.js
+++ .storybook/storybook.js
@@ -4,13 +4,14 @@ import registerScreen from "../app/navigator/register-screen";
 import { transparent } from "../app/navigator/styles";
 
 import { getStorybookUI, configure, addDecorator } from "@storybook/react-native";
+import { withKnobs } from "@storybook/addon-knobs/react";
 
 import React, { Component } from "react";
 import { Navigation } from "react-native-navigation";
 
 // This assumes that storybook is running on the same host as your RN packager,
 // to set manually use, e.g. host: 'localhost' option
-const StorybookUIRoot = getStorybookUI( { port: 7007, host: 'localhost' } );
+const StorybookUIRoot = getStorybookUI( { port: 7007, host: "localhost" } );
 
 // react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
 // https://github.com/storybooks/storybook/issues/2081
@@ -28,6 +29,7 @@ class StorybookUIHMRRoot extends Component {
 
 configure( () => {
     addDecorator( navigatorStyleDecorator( StorybookUIHMRRoot ) );
+    addDecorator( withKnobs );
     require( "./stories" );
 }, module );
 
