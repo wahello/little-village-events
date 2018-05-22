c7d0de21f979cd9d2a5d299b090d917fa392fdad
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-05-21 21:19:23 -0500

    Fix: details screen shows nothing for a second or two after opening

--- app/containers/event-details/event-details.state.js
+++ app/containers/event-details/event-details.state.js
@@ -1,24 +1,32 @@
-import { mergeIntoState, update } from "../../utils/freactal";
+import { mergeIntoState } from "../../utils/freactal";
 
 import axios from "axios";
 
-const loadEvent = async ( effects, { event } ) => {
-    await effects.setEvent( event );
+const initialize = async ( effects, { event } ) => {
+    effects.loadEvent( event );
+    return mergeIntoState( {
+        event
+    } );
 
+};
+
+const loadEvent = async ( effects, event ) => {
     const url = `http://littlevillagemag.com/iowa-city-area-events-calendar/events/${event.id}.json`;
     const { data } = await axios.get( url );
 
     return mergeIntoState( {
         event: data
     } );
+
 };
 
 export default {
     initialState: () => ( {
         event: null
     } ),
+
     effects: {
-        initialize: loadEvent,
-        setEvent: update( event => ( { event } ) )
+        initialize,
+        loadEvent
     }
 }
