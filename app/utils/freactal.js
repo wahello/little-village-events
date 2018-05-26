03d72666af9c78af31d4d625bb85b8373eca3de3
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-05-25 20:18:47 -0500

    WIP: add freactal logger

--- app/utils/freactal.js
+++ app/utils/freactal.js
@@ -7,9 +7,17 @@ import {
 
 import hoistNonReactStatics from "hoist-non-react-statics";
 
+const middleware = () => {
+    const result = [];
+    if ( __DEV__ ) {
+        const logger = require( "freactal-logger" ).default;
+        result.push( logger() );
+    }
+    return result;
+};
 
 export function provideState( state ) {
-    const wrapper = provideState_( state );
+    const wrapper = provideState_( { ...state, middleware: middleware() } );
     return ( Component ) => hoistNonReactStatics( wrapper( Component ), Component );
 }
 
