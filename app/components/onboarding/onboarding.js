c66fdb1dcd5b818739df9b4765b774cb65b3f08f
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-06-12 15:18:57 -0500

    WIP: Onboarding "Skip" buttons

--- app/components/onboarding/onboarding.js
+++ app/components/onboarding/onboarding.js
@@ -21,6 +21,7 @@ class OnboardingSequence extends Component {
     _swiperRef = React.createRef()
 
     next = () => this._swiperRef.current.scrollBy( 1 )
+    skip = n => this._swiperRef.current.scrollBy( n, false )
 
     render() {
         return (
@@ -30,10 +31,10 @@ class OnboardingSequence extends Component {
                 showsPagination={false}
             >
                 <OnboardingStart onContinue={ this.next } />
-                <InterestsIntro onContinue={ this.next } />
-                <InterestsPicker onContinue={ this.next } />
-                <LocationIntro onContinue={ this.next } />
-                <LocationPicker onContinue={ this.next } />
+                <InterestsIntro onContinue={ this.next } onSkip={ () => this.skip( 2 ) } />
+                <InterestsPicker onContinue={ this.next } onSkip={ () => this.skip( 1 ) } />
+                <LocationIntro onContinue={ this.next } onSkip={ () => this.skip( 2 ) } />
+                <LocationPicker onContinue={ this.next } onSkip={ () => this.skip( 1 ) } />
                 <NotificationsIntro />
             </Swiper>
         );
