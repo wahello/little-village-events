7ae1433c292b95fa4069966d6476bff4775e609e
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-06-26 14:38:36 -0500

    WIP: further saturate secondary highlight color

--- app/styles/variables.js
+++ app/styles/variables.js
@@ -21,7 +21,7 @@ export default {
     borderWidth: StyleSheet.hairlineWidth,
 
     highlightColor: "#007AFF",
-    highlightColorLight: "rgba(0,122,255,.15)",
+    highlightColorLight: "rgba(0,122,255,.1)",
     warningColor: "#FF0000",
 
     marginHorizontal: 10
